b662d2c5ef901cfa22d44427aa964f7ea5299e2c
Jesse McKenzie <jesse@jessemckenzie.com>
2023-05-02 11:57:39 -0700

    Changed nothing of substance, this is to test the git merge feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>{data && JSON.stringify(data)}</h1>
       <h1>{isPending && JSON.stringify(isPending)}</h1>
+      <h1>NAVBAR!!!</h1>
     </nav >
   );
 }
