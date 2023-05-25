e3948428835eac5bf4b185a8dee7e7a3bf70d127
Luke Chung <lukechung1020@gmail.com>
2023-05-25 10:23:10 -0700

    Fixed tab sizes to 2 for all js files

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -6,38 +6,38 @@ import { Confirmation, Navbar } from '../components';
 
 
 function ResetPassword() {
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const SendPasswordEmail = async () => {
-        await sendPasswordResetEmail(auth, email);
-        document.getElementById('confirm-modal').showModal();
-    }
+  const SendPasswordEmail = async () => {
+    await sendPasswordResetEmail(auth, email);
+    document.getElementById('confirm-modal').showModal();
+  }
 
-    const backToLogin = () => {
-        navigate('/login');
-    }
+  const backToLogin = () => {
+    navigate('/login');
+  }
 
-    return (
-        <>
-            <Navbar title='BarterBetter' backArrow={true} />
-            <div className='user-form'>
-                <h1>
-                    Request Password Reset
-                </h1>
-                <form onSubmit={event => event.preventDefault()}>
-                    <input type='email' placeholder='email'
-                        onChange={(event) => {
-                            setEmail(event.target.value);
-                        }}>
-                    </input>
-                    <button onClick={SendPasswordEmail}>Send</button>
-                </form>
-            </div>
-            <Confirmation onConfirm={backToLogin} id='confirm-modal' buttonMessage='Back to Login' title='An email has been sent! Please follow the instructions on the email to reset your password.'></Confirmation>
-        </>
-    )
+  return (
+    <>
+      <Navbar title='BarterBetter' backArrow={true} />
+      <div className='user-form'>
+        <h1>
+          Request Password Reset
+        </h1>
+        <form onSubmit={event => event.preventDefault()}>
+          <input type='email' placeholder='email'
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}>
+          </input>
+          <button onClick={SendPasswordEmail}>Send</button>
+        </form>
+      </div>
+      <Confirmation onConfirm={backToLogin} id='confirm-modal' buttonMessage='Back to Login' title='An email has been sent! Please follow the instructions on the email to reset your password.'></Confirmation>
+    </>
+  )
 
 }
 
