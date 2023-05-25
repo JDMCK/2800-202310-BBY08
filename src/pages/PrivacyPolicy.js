5736023ff60468c8adae7c4eea7e97879183298b
Jesse McKenzie <jesse@jessemckenzie.com>
2023-05-25 11:37:35 -0700

    Added a privacy policy.

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,60 @@
+import { Navbar } from "../components";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Navbar title='Privacy Policy' />
+      <div className='privacy-container'>
+
+        <p id='privacy-policy'>
+          Privacy Policy for Barter Better
+
+          Effective Date: May 25, 2023
+
+          Barter Better (referred to as "Barter Better," "we," or "our") is committed to protecting your privacy. This Privacy Policy explains how we collect, use, and disclose information when you access or use our web application ("WebApp"). By using Barter Better, you consent to the terms and practices outlined in this Privacy Policy.
+
+          1. Information We Collect:
+          1.1. Name: We collect your name to personalize your experience on Barter Better and to display it to other users if you choose to share it.
+
+          1.2. Email Address: We collect your email address to create and manage your account, communicate with you regarding account-related matters, and send you relevant notifications and updates.
+
+          1.3. Location: With your consent, we may collect your location information to provide location-based services and enhance your user experience within Barter Better. This information will not be shared with other users.
+
+          2. Use of Information:
+          2.1. Personalization: Your name is used to personalize your experience within Barter Better and display it to other users if you choose to share it.
+
+          2.2. Account Management: We use your email address to create and manage your account, send you important notifications, and communicate with you regarding account-related matters.
+
+          2.3. Communication: We may use your email address to respond to your inquiries, provide customer support, and send you relevant updates and promotional information about our services. You can opt out of promotional emails at any time.
+
+          2.4. Location-based Services: With your consent, we may use your location information to provide you with location-based features and services within Barter Better.
+
+          3. Information Sharing and Disclosure:
+          3.1. Other Users: If you choose to share your name within Barter Better, it will be visible to other users as part of your profile information.
+
+          3.2. Service Providers: We may engage trusted third-party service providers who assist us in operating Barter Better, delivering services, or conducting business activities on our behalf. These service providers have access to your personal information only to perform specific tasks and are obligated to maintain its confidentiality.
+
+          3.3. Legal Requirements: We may disclose your personal information if required to do so by law, in response to a valid legal request, or to protect our rights, property, or safety.
+
+          4. Data Security:
+          We implement reasonable security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. However, please note that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+
+          5. Data Retention:
+          We retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy unless a longer retention period is required or permitted by law.
+
+          6. Children's Privacy:
+          Barter Better is not intended for use by individuals under the age of 18. We do not knowingly collect personal information from children. If we become aware that we have unintentionally collected personal information from a child under the age of 18, we will take appropriate steps to delete it.
+
+          7. Changes to this Privacy Policy:
+          We may update this Privacy Policy from time to time. The most current version will be posted on Barter Better, and the effective date will be indicated. We encourage you to review this Privacy Policy periodically for any changes. Your continued use of Barter Better after any modifications to the Privacy Policy will signify your acceptance of the updated terms.
+
+          8. Contact Us:
+          If you have any questions, concerns, or requests regarding this Privacy Policy or the handling of your personal information, please contact us at info@barterbetter.ca.
+
+          By using Barter Better, you acknowledge that you have read and understood this Privacy Policy</p>
+      </div>
+    </>
+  );
+}
+
+export default PrivacyPolicy;
